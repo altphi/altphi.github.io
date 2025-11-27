49f422f16135e047c9b5b01d9920cca185b28dd9
Stephen Beck <2529298+altphi@users.noreply.github.com>
2025-11-27 10:01:08 -0500

    another day

--- sync-dailies.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-
-rsync -aP --delete ~/vaults/sdb/dailies/ ~/code/altphi.github.io/content/notes/
-rsync -aP --delete ~/vaults/clt/dailies/ ~/code/altphi.github.io/content/techne/
